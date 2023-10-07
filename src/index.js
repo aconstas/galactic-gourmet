7c8e952d7f06bdc7e92a1a11a5d9db4e5df85a9e
aconstas <82304192+aconstas@users.noreply.github.com>
2023-10-06 19:19:10 -0700

    add tab switching logic

--- src/index.js
+++ src/index.js
@@ -1,5 +1,39 @@
-import loadRestaurantInfo from "./components/Home/Home";
+import loadHome from "./components/Home/Home";
+import loadContact from "./components/Contact/Contact";
+import loadMenu from "./components/Menu/Menu";
 import '../src/components/Home/Home.css';
 
 console.log('Webpack is with you.');
-loadRestaurantInfo();
+loadHome();
+
+const contentDiv = document.getElementById('content');
+
+function clearContent() {
+    while (contentDiv.firstChild) {
+        contentDiv.firstChild.remove();
+    }
+}
+
+function setupTabs() {
+    const tabs = document.querySelectorAll('.tab');
+    console.log(tabs);
+    tabs.forEach(tab => {
+        tab.addEventListener('click', (e) => {
+            clearContent();
+
+            switch (e.target.id) {
+                case 'menu':
+                    loadMenu();
+                    break;
+                case 'home':
+                    loadHome();
+                    break;
+                case 'contact':
+                    loadContact();
+                    break;
+            }
+        });
+    });
+}
+
+setupTabs();
