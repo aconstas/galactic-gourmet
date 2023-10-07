87a8ac64797d45a9a3bdc5adc859a6a2be3267cb
aconstas <82304192+aconstas@users.noreply.github.com>
2023-10-07 12:15:04 -0700

    add roles, image alt attributes

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -6,6 +6,7 @@ export default function loadContact () {
     const contacts = [
         {
             name: "Zorblatt Nebulaflare",
+            role: "head chef",
             phone: "1-800-123-XENO",
             email: "zorblatt@milkwaymunchies.galaxy",
             catchphrase: "Cooking with a pinch of stardust and a dash of cosmic flair!",
@@ -13,6 +14,7 @@ export default function loadContact () {
         },
         {
             name: "Lirena Starbroker",
+            role: "owner",
             phone: "1-888-789-GALX",
             email: "lirena@cosmicventures.galaxy",
             catchphrase: "Transforming nebulous ideas into stellar successes!",
@@ -28,25 +30,29 @@ export default function loadContact () {
         contentDiv.appendChild(card);
     
         const name = document.createElement('p');
-        name.innerHTML = `<strong>${contact.name}</strong>`;
+        name.innerHTML = `<strong>${contact.name}</strong>, <em>${contact.role}<em>`;
         name.classList.add('name');
         card.appendChild(name);
 
         const profileImage = document.createElement('img');
         profileImage.classList.add('profile-image');
         profileImage.src = contact.image;
+        profileImage.alt = `${contact.name} profile image`;
         card.appendChild(profileImage);
     
         const phone = document.createElement('p');
         phone.innerHTML = `<strong>Phone:</strong> ${contact.phone}`;
+        phone.classList.add('phone');
         card.appendChild(phone);
     
         const email = document.createElement('p');
         email.innerHTML = `<strong>Email:</strong> ${contact.email}`;
+        email.classList.add('email');
         card.appendChild(email);
     
         const catchphrase = document.createElement('p');
         catchphrase.innerHTML = `<em>"${contact.catchphrase}"</em>`;
+        catchphrase.classList.add('catchphrase');
         card.appendChild(catchphrase);
         return contentDiv;
     })
