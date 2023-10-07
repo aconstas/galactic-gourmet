d34dc9368491963da353a160fca01cf70a4d6933
aconstas <82304192+aconstas@users.noreply.github.com>
2023-10-06 23:57:45 -0700

    initial commit, contact logic

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,3 +1,53 @@
+import ChefImage from '../../assets/images/chef.png';
+import OwnerImage from '../../assets/images/owner.png';
+
 export default function loadContact () {
-    console.log('Hey');
+
+    const contacts = [
+        {
+            name: "Zorblatt Nebulaflare",
+            phone: "1-800-123-XENO",
+            email: "zorblatt@milkwaymunchies.galaxy",
+            catchphrase: "Cooking with a pinch of stardust and a dash of cosmic flair!",
+            image: ChefImage,
+        },
+        {
+            name: "Lirena Starbroker",
+            phone: "1-888-789-GALX",
+            email: "lirena@cosmicventures.galaxy",
+            catchphrase: "Transforming nebulous ideas into stellar successes!",
+            image: OwnerImage,
+        }
+    ]
+
+    contacts.forEach(contact => {
+        const contentDiv = document.getElementById('content');
+
+        const card = document.createElement('div');
+        card.classList.add('container');
+        contentDiv.appendChild(card);
+    
+        const name = document.createElement('p');
+        name.innerHTML = `<strong>${contact.name}</strong>`;
+        name.classList.add('name');
+        card.appendChild(name);
+
+        const profileImage = document.createElement('img');
+        profileImage.classList.add('profile-image');
+        profileImage.src = contact.image;
+        card.appendChild(profileImage);
+    
+        const phone = document.createElement('p');
+        phone.innerHTML = `<strong>Phone:</strong> ${contact.phone}`;
+        card.appendChild(phone);
+    
+        const email = document.createElement('p');
+        email.innerHTML = `<strong>Email:</strong> ${contact.email}`;
+        card.appendChild(email);
+    
+        const catchphrase = document.createElement('p');
+        catchphrase.innerHTML = `<em>"${contact.catchphrase}"</em>`;
+        card.appendChild(catchphrase);
+        return contentDiv;
+    })
 }
