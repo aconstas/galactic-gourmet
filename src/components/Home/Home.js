d35b96ddef8cbadc517ae16ab10bc679be5a1fa7
Alex Constas <82304192+aconstas@users.noreply.github.com>
2023-12-11 20:04:09 -0800

    added alt text to images for accessability

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -16,6 +16,7 @@ export default function loadHome () {
 
     const restaurantImage = document.createElement('img');
     restaurantImage.src = RestaurantPic;
+    restaurantImage.alt = "Inside view of restaurant"
     reviewContainer.appendChild(restaurantImage);
     const review = document.createElement('p');
     review.innerText = "Absolutely out of this world, 'Galactic Gourmet' on planet Nebulon-5 is a dining experience like no other! Nestled on the shimmering plains of Crystalline Valley, this restaurant boasts a menu infused with the rarest ingredients from across the galaxy."
@@ -26,6 +27,7 @@ export default function loadHome () {
 
     const viewImage = document.createElement('img');
     viewImage.src = ViewPic;
+    viewImage.alt = "View from dining area";
     text1Container.appendChild(viewImage);
     const text1 = document.createElement('p');
     text1.innerText = "As you savor dishes that dance with cosmic flavors, large glass domes offer panoramic views of the breathtaking Nebulon skies, where meteor showers serve as your candlelight.";
@@ -36,6 +38,7 @@ export default function loadHome () {
 
     const dessertImage = document.createElement('img');
     dessertImage.src = Dessert;
+    dessertImage.alt = "Image of galactic dessert";
     text2Container.appendChild(dessertImage);
     const text2 = document.createElement('p');
     text2.innerText = "If the exquisite cuisine doesn't transport you to another dimension, the zero-gravity dessert room surely will. A trip to Galactic Gourmet is not just a meal; it's an interstellar journey for your taste buds!";
