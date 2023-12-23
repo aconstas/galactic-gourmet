1fa51a271e0e317816a00f43a6847e6f63a3c39d
aconstas <82304192+aconstas@users.noreply.github.com>
2023-12-22 20:30:59 -0800

    remove unused JavaScript

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import loadContact from "./components/Contact/Contact";
 import loadMenu from "./components/Menu/Menu";
 import '../src/components/Home/Home.css';
 
-console.log('Webpack is with you.');
 loadHome();
 
 const contentDiv = document.getElementById('content');
@@ -16,7 +15,6 @@ function clearContent() {
 
 function setupTabs() {
     const tabs = document.querySelectorAll('.tab');
-    console.log(tabs);
     tabs.forEach(tab => {
         tab.addEventListener('click', (e) => {
             clearContent();
